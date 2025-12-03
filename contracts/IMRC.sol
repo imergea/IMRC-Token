832698c56f199f2dfa116a5ea66c6395675fd45f
Imergea <info@imergea.com>
2025-12-03 11:40:08 +0100

    edited IMRC.sol
    Updated IMRC contract deployed

--- contracts/IMRC.sol
+++ contracts/IMRC.sol
@@ -1,13 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+/**
+ * -----------------------------------------------------------------
+ * IMRC Token – IMERGEA Consulting Coupon Token
+ * 1 IMRC = 1 Day of Strategic Consulting
+ *
+ * Services covered by 1 IMRC:
+ * - Raise Capital
+ * - Buy & Sell
+ * - M&A Advisory
+ * - Export Business Strategy
+ * - Business Valuation
+ * - Investor M&A Document Package
+ * - Investor Outreach
+ *
+ * Official Website: https://imergea.com
+ * -----------------------------------------------------------------
+ * Fixed supply: 100 IMRC
+ * Decimals: 18 (standard ERC-20)
+ * Owner receives all tokens at deployment
+ * No further minting possible (scarcity = value)
+ * -----------------------------------------------------------------
+ */
 
-/// @title IMRC – IMERGEA Credit Token
-/// @notice 1 IMRC = 1 day of IMERGEA strategic advisory + a unique referral code managed off-chain.
-contract IMRC is ERC20 {
-    constructor() ERC20("IMERGEA Credit", "IMRC") {
-        // Fixed supply: 100 IMRC (with 18 decimals)
-        _mint(msg.sender, 100 * 10 ** decimals());
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract IMRC is ERC20, Ownable {
+
+    // Public field visible on BaseScan
+    string public website = "https://imergea.com";
+
+    constructor()
+        ERC20("IMERGEA Consulting Coupon Token", "IMRC")
+        Ownable(msg.sender)
+    {
+        uint256 totalSupply = 100 * 10**decimals();
+        _mint(msg.sender, totalSupply);
     }
 }
